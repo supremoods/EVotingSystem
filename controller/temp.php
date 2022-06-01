1734b964ca8b2bcafb939729464f156a958c695f
supremoods <80272399+supremoods@users.noreply.github.com>
2022-06-01 12:11:39 +0800

    Update temp.php

--- controller/temp.php
+++ controller/temp.php
@@ -16,47 +16,5 @@
     // Create a new Database object
     $database = new Database();
 
-    // // Create a new UserAccount object
-    // $userAccount = new UserAccount(
-    //     "NULL", 
-    //     "student", 
-    //     "Technological University of the Philippines",
-    //     "TUPM-19-2343", 
-    //     "NULL",
-    //     "John", 
-    //     "Along", 
-    //     "Lappay", 
-    //     "john.lappay@tup.edu.ph", 
-    //     "2has982bns88i2ha", 
-    //     "NULL", 
-    //     "NULL", 
-    //     "NULL"
-    // );
-
-    // // Create a new SqlQuery object
-    // $sqlQuery = new SqlQuery(
-    //     $userAccount->id, 
-    //     $userAccount->userLevel, 
-    //     $userAccount->university, 
-    //     $userAccount->universityID, 
-    //     $userAccount->userID, 
-    //     $userAccount->first_name, 
-    //     $userAccount->middle_name, 
-    //     $userAccount->last_name, 
-    //     $userAccount->email, 
-    //     $userAccount->passw, 
-    //     $userAccount->status, 
-    //     $userAccount->time_stamp_in, 
-    //     $userAccount->time_stamp_out
-    // );
-
-
-    // // call the insertUserAccount method
-    //     $sqlQuery->insertUserAccount();
-
-        
-
-    // $sqlQuery->printAllUserAccounts();
-
 
 ?>
